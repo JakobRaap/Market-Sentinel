77af90fda1fc4872a63782e17365a623e40b80ec
Jakob Raap <bokaj.paar@gmail.com>
2023-05-05 13:17:29 +0200

    fetching and rendering of all events on current day

---
+++ pages/components/TodaysCard/index.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const StyledLi = styled.li`
+  list-style: none;
+  border: 1px solid black;
+  margin: 2px;
+  h2 {
+    font-size: 1rem;
+    margin-left: 5px;
+    font-weight: bold;
+
+    text-transform: uppercase;
+  }
+  p {
+    font-size: 0.8rem;
+    text-transform: uppercase;
+    margin: 2px;
+    margin-left: 8px;
+  }
+`;
+
+export default function TodaysCard({ event }) {
+  return (
+    <>
+      <StyledLi key={event.id}>
+        <h2>{event.title}</h2>
+        <p>{event.time}</p>
+        <p>{event.country}</p>
+        <p>{event.impact}</p>
+      </StyledLi>
+    </>
+  );
+}
