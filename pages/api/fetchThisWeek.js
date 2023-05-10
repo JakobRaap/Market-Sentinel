5b6a77eb1594e34ec7c055f08110ec7e3b79a000
Jakob Raap <bokaj.paar@gmail.com>
2023-05-10 10:49:03 +0200

    comments deleted

--- pages/api/fetchThisWeek.js
+++ pages/api/fetchThisWeek.js
@@ -1,11 +1,3 @@
-// export default async function fetchThisWeek(req, res) {
-//   const response = await fetch(
-//     "https://nfs.faireconomy.media/ff_calendar_thisweek.xml"
-//   );
-//   const xml = await response.text();
-//   await res.status(200).json(xml);
-// }
-
 import xml2js from "xml2js";
 
 async function parseXML(xml) {
